c95fe487d60b0e81d937f373e5582604af7b6fa2
deonash <avi.deonash@gmail.com>
2024-06-08 16:49:34 +0530

    Laxicograph order of List employees using stream

---
+++ Java/code/JavaStreams/src/com/techoral/basic/MaxSalaryLaxicograph.java
@@ -0,0 +1,78 @@
+package com.techoral.basic;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Author: www.techoral.com
+ * 
+ * Description : To create a tricky problem statement for finding the employee
+ * with the maximum salary greater than 30,000 using Java streams, you can add
+ * some subtle complexities. Here’s a modified problem statement:
+ */
+
+public class MaxSalaryLaxicograph {
+
+	static class Employee {
+		String name;
+		Double salary;
+
+		public Employee(String name, Double salary) {
+			this.name = name;
+			this.salary = salary;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Double getSalary() {
+			return salary;
+		}
+
+		public void setSalary(Double salary) {
+			this.salary = salary;
+		}
+
+		// getters and setters
+
+	}
+
+	public static void main(String[] args) {
+
+		List<Employee> employeeList = Arrays.asList(
+
+				new Employee("techoral", 45000.0), 
+
+			
+
+				new Employee("Avi", 55000.0),
+
+				new Employee("Kevin", 55000.0),
+				new Employee("Nash", 25000.0),
+
+				new Employee("guru", 15000.0),
+				new Employee("com", 35000.0)
+		);
+
+		Optional<Employee> empMax = employeeList.stream().filter(emp -> emp.getSalary() > 30000).sorted((a, b) -> {
+			int maxSal = Double.compare(b.getSalary(), a.getSalary());
+
+			if (maxSal == 0) {
+				return a.getName().compareTo(b.getName());
+			}
+			return maxSal;
+		}
+
+		).findFirst();
+		System.out.println(empMax.map(e -> "Empname with Max salary: " + e.getName() + " : " + e.getSalary())
+				.orElse("No Employee with greaterthan 30000"));
+
+	}
+
+}
